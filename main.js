29e6848b6f9131763dd7ad3fee1274c70e379e45
nickzoum <nzoumis@outlook.com>
2021-05-28 01:16:47 +0300

    test

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ app.get("/", function (request, response) {
     response.send("<h1>Test Home Page</h1>");
 });
 
+console.log(process.env.PORT);
+console.log(process.env.port);
+
 app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running");
 });
