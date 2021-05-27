97c33b6ab4f5d1933976ea04ff45da6cf11fcf2f
nickzoum <nzoumis@outlook.com>
2021-05-28 01:10:25 +0300

    testing heroku start

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var app = express();
-var port = process.env.PORT;
 
 app.use(express.static("scripts"));
 app.use(express.static("styles"));
@@ -9,6 +8,6 @@ app.get("/", function (request, response) {
     response.send("<h1>Test Home Page</h1>");
 });
 
-app.listen(port || 3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running");
 });
