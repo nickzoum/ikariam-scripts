04a6d53e5ddc1625c9754c036558fdb7d444dd13
nickzoum <nzoumis@outlook.com>
2021-05-28 01:47:36 +0300

    fix static file paths

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static("scripts"));
-app.use(express.static("styles"));
+app.use('/scripts', express.static('scripts'));
+app.use('/styles', express.static('styles'));
 
 app.get("/", function (request, response) {
     response.send("<h1>Test Home Page</h1>");
 });
 
-console.log(process.env.PORT);
-console.log(process.env.port);
-
-var port = process.env.PORT;
+var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-    console.log("Server is running port: " + port);
+    console.log("Server is running, port: " + port);
 });
