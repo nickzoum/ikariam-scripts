7ac5f79b3530b2f761ecfbb3f1f202b2e62422c2
nickzoum <nzoumis@outlook.com>
2021-06-08 20:05:38 +0300

    copy piracy improvement

--- scripts/copy-piracy.js
+++ scripts/copy-piracy.js
@@ -11,34 +11,63 @@
     buttonList.appendChild(button);
     button.id = "finder-button";
     button.addEventListener("click", function () {
-        var text = getPoints();
-        if (text) copyText(text);
-        else alert("Open fortress first");
+        getPoints().then(copyText);
     });
 
     function getPoints() {
-        return [].slice.call(document.querySelectorAll("#pirateHighscore>li")).map(function (li) {
-            var pointsDom = li.querySelector(".pirateBooty"), link = li.querySelector("a");
-            if (!pointsDom || !link) return;
-            return {
-                points: pointsDom.textContent.replace(/[^\d]/g, ""),
-                name: link.textContent,
-                isClose: link.parentElement.classList.contains("bold")
-            };
-        }).filter(Boolean).map(function (item) {
-            return item.name + ": " + item.points;
-        }).join("  ");
+        return new Promise(function (resolve, reject) {
+            getList().then(function (list) {
+                list = list.filter(function (item) {
+                    return item.cityId !== bgViewData.currentCityId;
+                }).map(function (item) {
+                    return item.name + ": " + item.capturePoints + " " + location.origin + "/?view=cityDetails&isMission=1&destinationCityId=" + item.cityId + "";
+                }).join("\n");
+                resolve("```\n" + list + "\n```");
+            }).catch(function (err) { reject(err); });
+        });
     }
 
     function copyText(text) {
-        var input = document.createElement("input");
+        var input = document.createElement("textarea");
         document.body.appendChild(input);
-        input.value = text;
+        input.textContent = text;
         input.focus();
         input.selectionStart = 0;
         input.selectionEnd = input.value.length;
         document.execCommand("copy");
         document.body.removeChild(input);
+    }
+
+
+    function getList() {
+        var city = bgViewData.currentCityId, request = ikariam.model.actionRequest;
+        return new Promise(function (resolve, reject) {
+            $.ajax({
+                url: "?view=pirateFortress&city=" + city + "&templateView=pirateFortress&backgroundView=city&position=17&ajax=1&activeTab=tabBootyQuest&currentCityId=" + city + "&actionRequest=" + request,
+                type: "POST",
+                error: function () { },
+                success: function (result) {
+                    if (typeof result === "string") {
+                        try { result = JSON.parse(result); }
+                        catch (err) { result = []; }
+                    }
+                    if (!(result instanceof Array)) return reject();
+                    result = result.find(function (item) {
+                        if (!(item instanceof Array)) return;
+                        return item[0] === "updateTemplateData";
+                    });
+                    if (!result) return reject();
+                    result = result[1];
+                    if (!result) return reject();
+                    result = result.load_js;
+                    if (!result) return reject();
+                    result = result.params;
+                    if (!result) return reject();
+                    try { resolve(JSON.parse(result).highscore); }
+                    catch (err) { reject(err); }
+                }
+            });
+        });
     }
 
 })();
