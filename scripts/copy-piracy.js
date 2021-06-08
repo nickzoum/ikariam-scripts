16aa0e065680f9b8b2961dcce8150039f23135e8
nickzoum <nzoumis@outlook.com>
2021-06-08 22:02:00 +0300

    copy piracy fix url

--- scripts/copy-piracy.js
+++ scripts/copy-piracy.js
@@ -20,7 +20,7 @@
                 list = list.filter(function (item) {
                     return item.cityId !== bgViewData.currentCityId;
                 }).map(function (item) {
-                    return "`" +item.name + ": " + item.capturePoints + "` " + location.origin + "/?view=cityDetails&isMission=1&destinationCityId=" + item.cityId + "";
+                    return "`" +item.name + ": " + item.capturePoints + "` " + location.origin + "/?view=island&cityId=" + item.cityId + "";
                 }).join("\n");
                 resolve(list);
             }).catch(function (err) { reject(err); });
