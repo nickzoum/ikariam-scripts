9bff1b7ba266b5b0de1fc4b8b6403d34e9e01432
nickzoum <nzoumis@outlook.com>
2021-06-10 21:57:09 +0300

    copy town first commit

---
+++ scripts/copy-town.js
@@ -0,0 +1,46 @@
+(function CopyTownIIFE() {
+
+    setInterval(updateBlock, 250);
+
+    function updateBlock() {
+        var oldBlock = document.querySelector("#js_selectedCityActionCopy>a");
+        if (oldBlock) return;
+        var reference = document.querySelector("#js_selectedCityAction10>a");
+        if (!reference) return;
+        var cityID = getLocation();
+        alert(cityID);
+        if (typeof cityID !== "string" || !cityID) return;
+        var text = location.origin + "/?view=island&cityId=" + cityID;
+        var newBlock = document.createElement("li");
+        newBlock.id = "js_selectedCityActionCopy";
+        newBlock.title = "Copy Link";
+        var link = document.createElement("a");
+        link.href = "about:blank";
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            copyText(text);
+        });
+        link.className = "smallFont";
+        link.appendChild(new Text("Copy Link"));
+        newBlock.appendChild(link);
+        reference.parentElement.parentElement.appendChild(newBlock);
+    }
+
+    function copyText(text) {
+        var input = document.createElement("input");
+        document.body.appendChild(input);
+        input.textContent = text;
+        input.focus();
+        input.selectionStart = 0;
+        input.selectionEnd = input.value.length;
+        document.execCommand("copy");
+        document.body.removeChild(input);
+    }
+
+    function getLocation() {
+        var link = document.querySelector(".cityLocation.city.selected>.link_img");
+        if (!(link instanceof HTMLAnchorElement) || typeof link.href !== "string") return;
+        var cityID = new URL(link.href).searchParams.get("cityId");
+        return typeof cityID === "string" ? cityID : null;
+    }
+})();
