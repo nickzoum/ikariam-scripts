5abe921e8b2c40eaa8683f20fc9b13873977f85a
nickzoum <nzoumis@outlook.com>
2021-07-08 14:49:39 +0300

    fix colors

--- scripts/colors.js
+++ scripts/colors.js
@@ -1,16 +1,31 @@
 (function ColorsIIFE() {
 
+    var allies = ["DARK", "ASN"], regex = /(DARK|ASN)$/;
+
     setInterval(updateBlocks, 250);
 
     function updateBlocks() {
         var citiDoms = document.querySelectorAll(".cityLocation.city:not(.cityLocationScroll):not(.recolored), .cityLocation.buildplace:not(.cityLocationScroll):not(.recolored)");
         var cities = ikariam.backgroundView.screen.data.cities;
-        if (citiDoms.length !== cities.length) return;
+        if (!cities || citiDoms.length !== cities.length) return;
         [...citiDoms].map(function (cityDom, index) {
             var cityInfo = cities[index] || {};
             cityDom.classList.add("recolored");
-            if (cityInfo.ownerAllyTag !== "DARK" && cityInfo.ownerAllyTag !== "ASN") return;
-            cityDom.classList.add("ally");
+            if (allies.includes(cityInfo.ownerAllyTag)) cityDom.classList.add("ally");
+        });
+        var fleets = document.querySelectorAll(".fleetAction.foreignBlocker:not(.recolored)");
+        var occupied = document.querySelectorAll(".occupied.foreignOccupier:not(.recolored)");
+        [...fleets].forEach(function (dom) {
+            dom.classList.add("recolored");
+            if (!regex.test(dom.title)) return;
+            dom.classList.remove("foreignBlocker");
+            dom.classList.add("allyBlocker");
+        });
+        [...occupied].forEach(function (dom) {
+            dom.classList.add("recolored");
+            if (!regex.test(dom.title)) return;
+            dom.classList.remove("foreignOccupier");
+            dom.classList.add("allyOccupier");
         });
     }
 })();
